41e1827e03deaf420ecf04133a23163eb8efaa35
anuj1012 <118649565+anuj1012@users.noreply.github.com>
2025-02-12 00:57:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// const sideMenu =document.querySelector('#sideMenu');
+// const navBar = document.getElementById("nav");
+// const navLinks = document.getElementById("ul");
+
+window.addEventListener('scroll', () => {
+    console.log(`Scroll position: ${window.scrollY}`);
+
+    if (window.scrollY > 50) {
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+    } else {
+        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
+    }
+});
+
+
+function openMenu(){
+    document.getElementById('sideMenu').style.right = '0';
+};
+function closeMenu(){
+   document.getElementById('sideMenu').style.right = '-100%';
+};
